86cef16fd289d27fc59ecfbde3626d6c64b86b49
Ismail Demir <id177992@gmail.com>
2022-04-02 10:24:02 -0400

    day02_second_third_classes

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -3,6 +3,6 @@ public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
         System.out.println("Adding a new code");
-        System.out.println("this is an other   ");
+        System.out.println("this is an other code");
     }
 }
---
+++ src/ThisJavaClass.java
@@ -0,0 +1,3 @@
+public class ThisJavaClass {
+    //This will also have some java code
+}
